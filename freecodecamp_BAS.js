05d5cf00f13b78dddcbe614030faf1ba9a7d57c9
Matt <mdark.math@gmail.com>
2016-03-15 15:47:34 -0700

    problem 2

--- freecodecamp_BAS.js
+++ freecodecamp_BAS.js
@@ -13,3 +13,33 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+
+
+//-------------------------------------Problem 2
+// Return the factorial of the provided integer.
+// If the integer is represented with the letter n, a factorial is the product of all positive integers less than or equal to n.
+// Factorials are often represented with the shorthand notation n!
+// For example: 5! = 1 * 2 * 3 * 4 * 5 = 120
+
+function factorialize(num) {
+  var multiply = [];
+  var factorial = 0;
+
+
+  if (num > 0) {
+    for (var i = 1; i <= num; i++) {
+      multiply.push(i);
+    }
+
+    factorial = multiply.reduce(function(a, b) {
+      return a * b;
+    });
+
+    return factorial;
+  } else {
+    return 1;
+  }
+}
+
+factorialize(5);
