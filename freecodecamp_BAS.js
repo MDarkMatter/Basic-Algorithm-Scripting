d6a76aaa68611466c709c8b67911bcddb0108cd9
Matt <mdark.math@gmail.com>
2016-03-15 13:38:32 -0700

    problem 1 done

--- freecodecamp_BAS.js
+++ freecodecamp_BAS.js
@@ -1,13 +1,15 @@
 // Reverse the provided string.
-//
 // You may need to turn the string into an array before you can reverse it.
-//
 // Your result must be a string.
 
 
-
 function reverseString(str) {
-  return str;
+  var array = [];
+
+  array = str.split('');
+  array.reverse();
+  array = array.join('');
+  return array;
 }
 
 reverseString("hello");
