aa3ec99f7269f3a06abf19bdc6ba3a4c90c4d02e
Matt <mdark.math@gmail.com>
2016-04-05 10:12:31 -0700

    Problem 3 complete

--- freecodecamp_BAS.js
+++ freecodecamp_BAS.js
@@ -43,3 +43,47 @@ function factorialize(num) {
 }
 
 factorialize(5);
+
+
+
+// ---------------------------------Problem 3
+// Check for Palindromes
+//
+// Return true if the given string is a palindrome. Otherwise, return false.
+//
+// A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+//
+// Note
+// You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
+//
+// We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
+
+
+function palindrome(str) {
+
+   var forwards = [];
+   var backwards = [];
+   var strfor;
+   var strback;
+   var newstr = str.replace(/\s/g, '');
+   var nonalph = newstr.replace(/[\W_]+/g, '');
+   var lower = nonalph.toLowerCase();
+
+   for (i = 0; i < lower.length; i++) {
+      forwards.push(lower[i]);
+   }
+
+   for (j = (lower.length - 1); j >= 0; j--) {
+      backwards.push(lower[j]);
+   }
+
+   strfor = forwards.join('');
+   strback =backwards.join('');
+
+   if (strfor === strback) {
+      return true;
+   } else {
+      return false;
+   }
+
+}
