8136d2fb39a7177513e2c54253fadcbe48fb26ba
Matt <mdark.math@gmail.com>
2016-04-07 11:37:58 -0700

    problem 7 complete

--- freecodecamp_BAS.js
+++ freecodecamp_BAS.js
@@ -139,3 +139,17 @@ function largestOfFour(arr) {
 
    return largestNum;
 }
+
+
+
+//------------Problem 7
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+  if (str.substr( (str.length-target.length) , target.length) === target){
+   return true;
+  } else {
+   return false;
+  }
+  
+}
