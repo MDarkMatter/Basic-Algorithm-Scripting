376979df88c7f0a49f63b376f823153e69c50b61
Matt <mdark.math@gmail.com>
2016-04-07 10:11:06 -0700

    problem 6 complete

--- freecodecamp_BAS.js
+++ freecodecamp_BAS.js
@@ -123,3 +123,19 @@ function titleCase(str) {
    var done = upper.join('');
    return done;
 }
+
+
+//-----------Problem 6
+// Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+// Remember, you can iterate through an array with a simple for loop, and access each member with array syntax arr[i].
+
+function largestOfFour(arr) {
+   var largestNum = [];
+
+   for (var i = 0; i < arr.length; i++) {
+      largestNum.push(Math.max.apply(null, arr[i]));
+   }
+
+   return largestNum;
+}
