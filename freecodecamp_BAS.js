d26e995e72322fd550dadc8f0e42921c512c5d88
Matt <mdark.math@gmail.com>
2016-04-12 15:57:29 -0700

    Problem 12 done

--- freecodecamp_BAS.js
+++ freecodecamp_BAS.js
@@ -212,3 +212,27 @@ function slasher(arr, howMany) {
   var arrSplice =  arr.splice(0, (howMany));
   return arr;
 }
+
+
+//-------------------Problem 12
+// Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+// For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+// The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+// Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+
+function mutation(arr) {
+   var zero = arr[0].toLowerCase();
+   var one = arr[1].toLowerCase();
+
+   for (var i = 0; i < one.length; i++){
+      if (zero.indexOf(one.charAt(i)) === -1){
+         return false;
+      }
+   }
+   for (var i = 0; i < one.length; i++){
+     if (zero.indexOf(one.charAt(i)) > 0){
+         return true;
+      }
+   }
+
+}
